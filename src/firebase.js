241370ce81fe173597ea5524c16fec79965b9729
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-09-08 22:00:55 +0200

    created amazon header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbgI7CMSsfHXHkowc54yoBdexY8pCh0UA",
+    authDomain: "clone-b2521.firebaseapp.com",
+    databaseURL: "https://clone-b2521.firebaseio.com",
+    projectId: "clone-b2521",
+    storageBucket: "clone-b2521.appspot.com",
+    messagingSenderId: "425098752089",
+    appId: "1:425098752089:web:db4fb4f0f9556179e28148",
+    measurementId: "G-F2NVGCHC74"
+};
