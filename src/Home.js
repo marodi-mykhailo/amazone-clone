c13053e0659abcf7c17314a18c5c619958d327d9
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-09-08 23:50:45 +0200

    create Product, Checkout, Subtotal components

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Home.css';
+import Product from "./Product";
 
 
 function Home() {
@@ -7,9 +8,39 @@ function Home() {
         <div className={'home'}>
             <div className={'home__container'}>
                 <img className={'home__image'}
-                    src={'https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'}
-                    alt={"amazonBg"}
+                     src={'https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'}
+                     alt={"amazonBg"}
                 />
+                <div className={'home__row'}>
+                    <Product id={'2111221'}
+                        title={"theaddasdnamdnbabdavdasdadva ah sdbasd babf abfba b" +
+                        "adasdasdadasdaffanfk jna fab fbaeb hfhs lean statup"} price={29.99}
+                             image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+                             rating={5}/>
+                    <Product title={"the lean statup"} price={29.99}
+                             image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+                             rating={5}/>
+                </div>
+                <div className={'home__row'}>
+                    <Product id={'212313123'}
+                        title={"the lean statup"} price={29.99}
+                             image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+                             rating={5}/>
+                    <Product id={'2123123'}
+                             title={"the lean statup"} price={29.99}
+                             image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+                             rating={5}/>
+                    <Product id={'2713123'}
+                             title={"the lean statup"} price={29.99}
+                             image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+                             rating={5}/>
+                </div>
+                <div className={'home__row'}>
+                    <Product id={'216513123'}
+                             title={"the lean statup"} price={29.99}
+                             image={'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'}
+                             rating={5}/>
+                </div>
             </div>
         </div>
     )
